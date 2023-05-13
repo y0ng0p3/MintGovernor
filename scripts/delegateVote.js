499c69b36dbb48da7a0f07d6f7ae2f0f5a0fc686
y0ng0p3 <vaillantyomen@gmail.com>
2023-05-13 10:49:12 +0100

    delegate the votes to myself (me which is also token owner)

---
+++ scripts/delegateVote.js
@@ -0,0 +1,14 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  me = ethers.getSigner("0x28Edfc28Fc9402a7B5c43A33713895C6aA5e8387");
+  governorAddress = "0x88a7dC0ad3a3af8Cb6BEb8B19179b2Be49bBB088";
+  tokenAddress = "0xbA65B000c3A345efCd62E9778cC0aA402BB4A32a";
+
+  vovernor = ethers.getContractAt("Vovernor", governorAddress);
+  voken = ethers.getContractAt("Voken", tokenAddress);
+
+  await voken.delegate(me.address);
+}
+
+main();
